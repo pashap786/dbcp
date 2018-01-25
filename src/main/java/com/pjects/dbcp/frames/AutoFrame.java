dbe4e775549f0f34fa47c1645dfce6a152228e85
Pasha Utt <putt@healthesystems.com>
2018-01-25 11:35:34 -0500

    asdf

--- src/main/java/com/pjects/dbcp/frames/AutoFrame.java
+++ src/main/java/com/pjects/dbcp/frames/AutoFrame.java
@@ -2,9 +2,6 @@ package com.pjects.dbcp.frames;
 
 public class AutoFrame extends javax.swing.JFrame {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 }
--- src/main/java/com/pjects/dbcp/frames/OrderFrame.java
+++ src/main/java/com/pjects/dbcp/frames/OrderFrame.java
@@ -2,9 +2,6 @@ package com.pjects.dbcp.frames;
 
 public class OrderFrame extends javax.swing.JFrame {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 }
